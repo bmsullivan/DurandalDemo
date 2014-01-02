a89bbc25b244bc867730ac3f34f6d34fd203191d
Brian Sullivan <bmsullivan@gmail.com>
2014-01-01 21:22:06 -0600

    Knockout demo done

--- DurandalDemo/Scripts/main.js
+++ DurandalDemo/Scripts/main.js
@@ -1,7 +1,35 @@
-﻿define(function(require) {
+﻿requirejs.config({    
+   paths: {
+       'knockout': 'knockout-3.0.0'
+   } 
+});
 
-    var MyLibrary = require('myLibrary');
-    var lib = new MyLibrary();
 
-    lib.showMessage();
+define(function(require) {
+
+    var ko = require('knockout');
+    
+    var ViewModel = function() {
+        var self = this;
+
+        self.message = ko.observable('foo');
+
+        self.changeMessage = function() {
+            self.message('bar');
+        };
+
+        self.things = ko.observableArray([{ text: 'first' }, { text: 'second' }, { text: 'third' }]);
+
+        self.addThing = function() {
+            self.things.push({ text: 'another' });
+        };
+
+        self.numberOfThings = ko.computed(function() {
+            return self.things().length;
+        });
+    };
+
+    var vm = new ViewModel();
+
+    ko.applyBindings(vm);
 });
